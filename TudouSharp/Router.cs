258494a88ff7a92acb435a005e519b6bb8f25a50
lersh <lersh99@gmail.com>
2018-01-19 17:49:59 +0800

    fix

--- TudouSharp/Router.cs
+++ TudouSharp/Router.cs
@@ -6,13 +6,43 @@ namespace TudouSharp
 {
     public class Router
     {
-        static string TudouHead = "佛曰：";
-        static string Tudou2Head = "如是我闻：";
+        static private string TudouHead = "佛曰：";
+        static private string Tudou2Head = "如是我闻：";
 
-        public static int GetCode(string TudouString)
+        private struct TudouBody
         {
-
-            return 0;
+            public int CodeMethod;
+            public string CodeString;
         }
+        private static TudouBody GetCode(string TudouString)
+        {
+            TudouBody body;
+            if (TudouString.Length < 3)
+            {
+                body.CodeMethod = -1;
+                body.CodeString = String.Empty;
+                return body;
+            }
+            else if (TudouString.Substring(0, TudouHead.Length) == TudouHead)
+            {
+                body.CodeMethod = 1;
+                body.CodeString = TudouString.Substring(TudouHead.Length);
+                return body;
+            }
+            else if (TudouString.Substring(0, Tudou2Head.Length) == Tudou2Head)
+            {
+                body.CodeMethod = 2;
+                body.CodeString = TudouString.Substring(Tudou2Head.Length);
+                return body;
+            }
+            else
+            {
+                body.CodeMethod = 0;
+                body.CodeString = String.Empty;
+                return body;
+            }
+        }
+
+
     }
 }
